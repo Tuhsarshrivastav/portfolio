48045997710c0c98aba38c85ea6b7b6dada14df7
Tusharshrivastav <ts3657@gmail.com>
2021-10-14 12:32:29 +0530

    changes added

--- src/Data/About_data.js
+++ src/Data/About_data.js
@@ -11,12 +11,20 @@ const Aboutskills = [
   },
   {
     title: "Fullstack Development",
-    about: "I can develop fullstack web application using mern-stack/mean-stack",
+    about:
+      "I can develop fullstack web application using mern-stack/mean-stack",
   },
   {
     title: "MobileApp Development",
-    about:
-      "i can build android and ios mobile apps using react-native ",
+    about: "i can build android application and ios application  using react-native ",
+  },
+  {
+    title: "Desktop Development",
+    about: "i can build windows linux and mac softwares application using electron.js ",
+  },
+  {
+    title: "UI/Ux Designing",
+    about: "i can create interactive web mobile  templates using figma/canva softwares ",
   },
 ];
 export { Aboutskills };
